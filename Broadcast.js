e6ba64cabf665f1d5542662520a1fd0d1a1af15e
miiibdboy <50560477+miiibdboy@users.noreply.github.com>
2019-05-13 02:47:32 +0200

    Add files via upload

---
+++ Broadcast.js
@@ -0,0 +1,48 @@
+﻿const Discord = require("discord.js");
+  const kboosh = new Discord.Client();
+    const kbooshtoken = "NTc0MDMzMzQwMzQ5Mjg0NDEy.XMzjRw.YFJg7R5Acb2iBzxo5Z3m0sczQQU"
+      kboosh.on('ready', () => {
+        kboosh.user.setGame(`♛ 🌜🌃رمضان كريم�`,'https://www.twitch.tv/TEST-Broadcast');
+          console.log('Im Ready!');
+  
+        });
+
+  kboosh.on('message', message => {
+    if (message.content.split(' ')[0] == '%k')
+       message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+
+           member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
+                                                      message.delete();
+            
+                                                    });
+            
+                                                  });
+   kboosh.on("message", message => {
+       var prefix = "%";
+ 
+             var args = message.content.substring(prefix.length).split(" ");
+                if (message.content.startsWith(prefix + "b")) {
+                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+                          if (!args[1]) {
+                            
+                                 let embed3 = new Discord.RichEmbed()
+                                     .setDescription(":white_check_mark: | تم ارسال رسالة لا يوجد فيها شيء")
+                                       .setColor("#FF00FF")
+                                          message.channel.sendEmbed(embed3);
+                            
+                                        } else {
+
+                            
+                                           let embed4 = new Discord.RichEmbed()
+                                                            .setDescription(':white_check_mark: | تم ارسال الرساله للجميع ..')
+                                                                .setColor("#99999")
+                               
+                                                                message.channel.sendEmbed(embed4);
+                                                      message.delete();
+                            }
+                          }
+});
+kboosh.login(kbooshtoken);
